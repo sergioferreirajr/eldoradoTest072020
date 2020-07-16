f0492e5c2bc4c2d0800adb9ee617c9e161ae624c
Sérgio Ferreira Jr <sergio.junior@citsamazonas.org.br>
2020-07-16 02:55:31 -0400

    Lista de categorias

--- deviceMng/src/app/category/categorylist.component.ts
+++ deviceMng/src/app/category/categorylist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Category } from './category.model';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.categories = this.getCategories();
   }
 
+  getCategories(): Category[]{
+    return this.dataService.getCategories();
+  }
+
+  deleteCategory($event: any, category: Category): void {
+    $event.preventDefault();
+    if (confirm('ATTENTION. This action can deletes all Devices related to this Category. Do you confirm the deletion: Category = "' + category.id + '"?')) {
+      this.dataService.deleteCategory(category.id);
+      this.categories = this.dataService.getCategories();
+    }
+  }
 }
