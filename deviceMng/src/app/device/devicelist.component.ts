eef377d7870409037b892181ba4d03bc239074ad
Sérgio Ferreira Jr <sergio.junior@citsamazonas.org.br>
2020-07-16 02:56:51 -0400

    Lista de Devices

--- deviceMng/src/app/device/devicelist.component.ts
+++ deviceMng/src/app/device/devicelist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Device } from './device.model';
+import { Category } from '../category/category.model';
 
 @Component({
   selector: 'app-devicelist',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceListComponent implements OnInit {
 
-  constructor() { }
+  devices: Device[];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.devices = this.getDevices();
   }
 
+  getDevices(): Device[]{
+    return this.dataService.getDevices();
+  }
+
+  getCategoryNameById(id: number): string {
+    const category: Category = this.dataService.getCategoryById(id);
+    return category.name;
+  }
+
+  deleteDevice($event: any, device: Device): void {
+    $event.preventDefault();
+    if (confirm('Do you confirm the deletion: Device = "' + device.id + '"?')) {
+      this.dataService.deleteDevice(device.id);
+      this.devices = this.dataService.getDevices();
+    }
+  }
 }
