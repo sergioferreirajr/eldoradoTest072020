85c1f16d8740b91c0f734894909710070be53e36
Sérgio Ferreira Jr <sergio.junior@citsamazonas.org.br>
2020-07-16 02:58:09 -0400

    Criando service pra armazenar no localstorage

--- deviceMng/src/app/services/data.service.ts
+++ deviceMng/src/app/services/data.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Device } from '../device/device.model';
 import { Category } from '../category/category.model';
 
@@ -8,41 +7,91 @@ import { Category } from '../category/category.model';
 })
 export class DataService {
 
-  readonly deviceValues = [
-    [1,1,'blue','1'],
-    [2,1,'red','2'],
-    [3,2,'green','3']
-  ];
-
-  readonly categoryValues = [
-    [1,'category 1'],
-    [2,'category 2'],
-    [3,'category 3']
-  ];
-
-  constructor() { }
+  constructor() {
+  }
 
   /**
    * Get all devices from database
    *
-   * @returns Observable<any>
+   * @returns Device[]
    */
-  public getDevices(): Observable<any>  {
-    return new Observable(obs => {
-      obs.next(this.deviceValues);
-      obs.complete();
-    });
+  public getDevices(): Device[]  {
+    const devices = localStorage['devices'];
+    return devices ? JSON.parse(devices): [];
+  }
+
+  /**
+   * Add new Device
+   * @param device
+   */
+  public newDevice(device: Device): void{
+    const devices = this.getDevices();
+    device.id = new Date().getTime();
+    devices.push(device);
+    localStorage['devices'] = JSON.stringify(devices);
+  }
+
+  /**
+   * Delete a device using an ID
+   */
+  public deleteDevice(id: number): void {
+    let devices: Device[] = this.getDevices();
+    devices = devices.filter(device => device.id !== id);
+    localStorage['devices'] = JSON.stringify(devices);
   }
 
   /**
    * Get all categories from database
    *
-   * @returns Observable<any>
+   * @returns Category[]
    */
-  public getCategories(): Observable<any>  {
-    return new Observable(obs => {
-      obs.next(this.categoryValues);
-      obs.complete();
-    });
+  public getCategories(): Category[]  {
+    const categories = localStorage['categories'];
+    return categories ? JSON.parse(categories): [];
+  }
+
+  /**
+   * Add new Category
+   * @param Category
+   */
+  public newCategory(category: Category): void{
+    const categories = this.getCategories();
+    category.id = new Date().getTime();
+    categories.push(category);
+    localStorage['categories'] = JSON.stringify(categories);
+  }
+
+  /**
+   * Delete a category using an ID
+   */
+  public deleteCategory(id: number): void {
+
+    let devices: Device[] = this.getDevicesByCategoryId(id);
+    for (var device of devices) {
+      this.deleteDevice(device.id);
+    }
+
+    let categories: Category[] = this.getCategories();
+    categories = categories.filter(category => category.id !== id);
+    localStorage['categories'] = JSON.stringify(categories);
+  }
+
+  /**
+   * Search Category by id
+   * @param id Number
+   */
+  public getCategoryById(id: number): Category {
+    const categories: Category[] = this.getCategories();
+    return categories.find(category => category.id == id);
+  }
+
+  /**
+   *
+   * @param id Search devices by Category Id
+   */
+  private getDevicesByCategoryId(id: number): Device[]{
+    let devices: Device[] = this.getDevices();
+    devices = devices.filter(device => device.categoryId == id);
+    return devices;
   }
 }
