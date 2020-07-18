de2a586e4e1ba714c179f40ca1887424d072ed49
Sérgio Ferreira Jr <sergio.junior@citsamazonas.org.br>
2020-07-18 06:35:19 -0400

    correção do erro CORS

--- deviceMngAPI/config/custom-express.js
+++ deviceMngAPI/config/custom-express.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+const cors = require('cors'); 
 
 module.exports = function(){
     var app = express();
+    app.use(cors());
     
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
