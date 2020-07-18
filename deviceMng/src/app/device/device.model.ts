1d06f1ef909287d7125d16003bb26a372e745e20
Sérgio Ferreira Jr <sergio.junior@citsamazonas.org.br>
2020-07-18 06:34:19 -0400

    padronizando nome do attr

--- deviceMng/src/app/device/device.model.ts
+++ deviceMng/src/app/device/device.model.ts
@@ -1,7 +1,7 @@
 export class Device {
   constructor (
     public id?: number,
-    public categoryId?: number,
+    public category_id?: number,
     public color?: string,
     public partNumber?:number
   ){}
