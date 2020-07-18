d9d54452b166c9bf5879a701814fab291ba65638
Sérgio Ferreira Jr <sergio.junior@citsamazonas.org.br>
2020-07-18 06:35:08 -0400

    finalizando API device

--- deviceMngAPI/controllers/devices.js
+++ deviceMngAPI/controllers/devices.js
@@ -3,14 +3,26 @@ const { check, validationResult } = require('express-validator');
 module.exports = function(app){
     //GET ALL DEVICES
     app.get('/devices', function(req, res){
-        console.log('Recebida a requisição de DEVICES na porta 3000');
-        res.send('OK');
+
+        var connection = app.persistence.connectionFactory();
+        var deviceDao = new app.persistence.DeviceDao(connection);
+
+        console.log('Listing devices...');
+        deviceDao.getAll(function(error, result){
+            if(error){
+                console.log('ERROR: List devices... ' + error);
+                res.status(400).send(error);
+            } else {
+                console.log("SUCCESS: Listing devices... ");
+                res.status(200).send(result)
+            }
+        });
+        connection.end();
     });
 
     //NEW DEVICE
     app.post('/devices/newdevice', 
     [
-        check('id', 'The ID is required').notEmpty(),
         check('category_id', 'The CATEGORY_ID is required.').notEmpty(),
         check('color', 'The COLOR must have only letters').custom((value, {req}) => /^[a-zA-Z]+$/.test(value)),
         check('color', 'The COLOR must have maximum 16 letters').custom((value, {req}) => value.length <= 16),
@@ -18,6 +30,10 @@ module.exports = function(app){
     ],
     function(req, res){
         var device = req.body;
+        var categoryId = req.body.category_id;
+        var color = req.body.color;
+        var partNumber = req.body.partNumber;
+        // var device = JSON.parse(req.body);
         
         var errors = validationResult(req);
         
@@ -31,15 +47,36 @@ module.exports = function(app){
         var connection = app.persistence.connectionFactory();
         var deviceDao = new app.persistence.DeviceDao(connection);
 
-        deviceDao.newDevice(device, function(error, result){
+        deviceDao.newDevice(device, categoryId, color, partNumber, function(error, result){
             if(error){
                 console.log('ERROR: New Device creation error: ' + error);
                 res.status(400).send(error);
             } else {
-                console.log("SUCCESS: New Device criated at database");
+                console.log("SUCCESS: New Device criated at database with ID: " + result.insertId);
                 res.status(201).json(device)
             }
         });
+
+        connection.end();
+    });
+
+    //DELETE CATEGORY BY ID
+    app.delete("/devices/:id", function(req, res){
+        var devideId = req.params.id;
+
+        var connection = app.persistence.connectionFactory();
+        var deviceDao = new app.persistence.DeviceDao(connection);
+
+        deviceDao.delete(devideId, function(error){
+            if(error){
+                console.log('ERROR: Device ID: ' + devideId + " was not deleted. " + error);
+                res.status(400).send(error);
+            } else {
+                console.log("SUCCESS: Device deleted from database: " + devideId);
+                res.status(200).json(devideId)
+            }
+        });
+        connection.end();
     });
 }
 
--- deviceMngAPI/persistence/DeviceDao.js
+++ deviceMngAPI/persistence/DeviceDao.js
@@ -2,12 +2,18 @@ function DeviceDao(connection){
     this._connection = connection;
 }
 
-DeviceDao.prototype.newDevice = function(device, callback){
-    this._connection.query('INSERT INTO device SET ?', device, callback)
+DeviceDao.prototype.newDevice = function(device, category_id, color, partNumber, callback){
+    let sqlTxt = 'INSERT INTO device (ID, CATEGORY_ID, COLOR, PARTNUMBER) VALUES (LAST_INSERT_ID(), ?, ?, ?)';
+    let values = [category_id, color, partNumber];
+    this._connection.query(sqlTxt, values, callback)
 }
 
 DeviceDao.prototype.getAll = function (callback) {
     this._connection.query("SELECT * FROM device",callback);
+}
+
+DeviceDao.prototype.delete = function (id, callback) {
+    this._connection.query("DELETE FROM device WHERE ID = ?", [id], callback);
 }
 
 module.exports = function(connection){
